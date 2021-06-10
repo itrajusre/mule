32f5b27187ac99c2a8f55ea91f7fa7312df93b3e
itrajusre <iraju2807@gmail.com>
2021-06-10 10:14:15 +0530

    Change the flow names

---
+++ javamodule/src/main/java/com/itraju/StockQuoteService.java
@@ -0,0 +1,24 @@
+package com.itraju;
+
+public class StockQuoteService 
+{
+	public static String sayHello(String name,String city)
+	{
+		return "Hello "+name+" from "+city;
+	}
+	public String getStockPrice(String stockSymbol)
+	{
+		if(stockSymbol.equals("mule"))
+		{ 
+			return "100";
+		} 
+		else if(stockSymbol.equals("ibm"))
+		{
+			return "200";
+		}
+		else
+		{
+			return " Stock doesn't Exist";
+		}
+	}
+}
